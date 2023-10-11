d73c64d957265c59250c607bd830d73fab206a25
hasirali <hasira804@gmail.com>
2023-10-11 18:49:25 +0530

    done

---
+++ Card/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Login from './component/Login'
+import Profile from './component/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>Login Page</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
